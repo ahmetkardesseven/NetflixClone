0f160de62b16d1fc35e0aaf071928c487b80aa36
ahmetkardesseven <ahmetkardesseven@gmail.com>
2023-02-03 02:17:42 +0300

    Extensions yazılacak.

--- NetflixClone/Controllers/HomeViewController.swift
+++ NetflixClone/Controllers/HomeViewController.swift
@@ -29,6 +29,8 @@ class HomeViewController: UIViewController {
         
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
         homeFeedTable.tableHeaderView = headerView
+        
+        getTrendingMovies()
     }
         private func configureNavbar() {
             var image = UIImage(named: "logo")
@@ -47,7 +49,19 @@ class HomeViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds
-        
+    }
+    private func getTrendingMovies(){
+        APICaller.shared.getTrendingMovies{ results in
+            switch results {
+                
+            case .success(let movies):
+                print(movies)
+            case .failure(let error):
+                print(error)
+            }
+            
+            
+        }
         
     }
 
---
+++ NetflixClone/Resources/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  NetflixClone
+//
+//  Created by ahmet kardesseven on 3.02.2023.
+//
+
+import Foundation
