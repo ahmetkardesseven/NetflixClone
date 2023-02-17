463b4566766af6e5f90b6bc049f4f3a01ce655e0
ahmetkardesseven <103737133+ahmetkardesseven@users.noreply.github.com>
2023-02-17 15:02:37 +0300

    Bitti
    NetfilxClone

---
+++ NetflixClone-main/NetflixClone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  TitleViewModel.swift
+//  NetflixClone
+//
+//  Created by ahmet kardesseven on 5.02.2023.
+//
+
+import Foundation
+
+
+struct TitleViewModel {
+    
+    let titleName: String
+    let posterURL: String
+    
+    
+}
